f959c775552af4449a0431dbd1eb4d193b4d368b
Tpoppp <olson0002@yahoo.com>
2020-04-04 13:46:11 -0700

    added JS

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-$("div.form-group").text();
-
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=EJBRr5XsjhnaGa8fZ87bNs1mfrv2sRYj";
-
-$
-.ajax({
-
-    url: queryURL,
-    method: "GET"
-
-}).then(function(response) {
+$(document).ready(function() {
     
+    $("#submit").on("click", function (){
+
+        let userSearch = $("#exampleFormControlInput1").val();
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearch + "&api-key=EJBRr5XsjhnaGa8fZ87bNs1mfrv2sRYj";
+        $
+        .ajax({
+        
+            url: queryURL,
+            method: "GET"
+        
+        }).then(function(response) {
+            console.log(response);
+     
+           
+
+    })
+    
+
+})
+});
+
+
+
+
+
+
